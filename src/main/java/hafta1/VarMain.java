c53180355ef27cc30c7116c3c9939e0b0b35abde
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-04 16:20:25 +0300

    1. I learned to swap numbers 2. I learned degree conversion

---
+++ src/main/java/hafta1/VarMain.java
@@ -0,0 +1,25 @@
+package hafta1;
+
+public class VarMain {
+
+    public static void main(String[] args) {
+
+        int var = 123; //
+
+        //String void -"Patika"; void ile değişken tanımı yapılamaz
+
+        var name = "Cem";
+
+        var age = 18;
+
+        //var value; geçersiz kullanım çünkü ilk değer ataması yok
+
+        var value = 10;
+
+        //value "Patika"; compile time hatası. çünkü bir üst satırda int olduğunu belirttik.
+
+        name = "Patika";
+
+
+    }
+}
