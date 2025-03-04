9caeb0a230a88d40bf56e9cf97000ae10c31b742
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-04 15:44:20 +0300

    Initial commit

---
+++ src/main/java/hafta1/JavaEgıtımı/SwapNumbersExample.java
@@ -0,0 +1,4 @@
+package hafta1.JavaEgıtımı;
+
+public class SwapNumbersExample {
+}
---
+++ src/main/java/hafta1/JavaEgıtımı/TemelPratiklerScanner.java
@@ -0,0 +1,25 @@
+package hafta1.JavaEgıtımı;
+
+import java.util.Scanner;
+
+public class TemelPratiklerScanner {
+
+    public static void main(String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Adınızı Giriniz: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Yaşınızı Giriniz: ");
+        int age = scanner. nextInt();
+
+        System.out.println("Merhaba " + name + ", " + age + " yaşındasın!");
+
+        scanner.close();
+
+
+
+
+    }
+}
