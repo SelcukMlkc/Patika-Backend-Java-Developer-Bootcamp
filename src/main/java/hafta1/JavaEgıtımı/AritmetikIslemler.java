dfa3986cdf9af4a901af2eeff9d5855ac1aeb986
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-05 23:23:20 +0300

    I added Arithmetic Operations

---
+++ src/main/java/hafta1/JavaEgıtımı/AritmetikIslemler.java
@@ -0,0 +1,25 @@
+package hafta1.JavaEgıtımı;
+
+import java.util.Scanner;
+
+public class AritmetikIslemler {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in) ;
+
+        System.out.println("a :");
+        int a = scanner.nextInt();
+        System.out.println("b :");
+
+        int b = scanner.nextInt();
+
+        System.out.println("c :");
+        int c = scanner.nextInt();
+
+        int sonuç = (a + b * c - b);
+        System.out.println("Sonuç: " + sonuç);
+
+
+    }
+}
