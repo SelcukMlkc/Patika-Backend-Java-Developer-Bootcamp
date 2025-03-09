5b57e5849df96eef51dad386e3636ee332257bf1
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-09 15:42:00 +0300

    I made a small change

--- src/main/java/hafta1/projects/FlightTicket.java
+++ src/main/java/hafta1/projects/FlightTicket.java
@@ -9,49 +9,46 @@ public class FlightTicket {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Mesafeyi km türünden giriniz: ");
-        double Mesafe = scanner.nextDouble();
+        double distance = scanner.nextDouble();
 
         System.out.println("Yaşınızı giriniz :");
-        double Yaş = scanner.nextDouble();
+        double age = scanner.nextDouble();
 
         System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ):");
-        double Tip = scanner.nextDouble();
+        double direction = scanner.nextDouble();
 
-        double fiyat = Mesafe * 0.10;
-        if (Tip == 2) {
+        double price = distance * 0.10;
+        if (direction == 2) {
             // bu durumda %20 indirim uygulanıyor
-            fiyat = fiyat * 2 * 0.8;
+            price = price * 2 * 0.8;
         }
 
-
-        if (Mesafe < 0 || Yaş < 0 || (Tip != 1 && Tip != 2)) {
+        if (distance < 0 || age < 0 || (direction != 1 && direction != 2)) {
             System.out.println("Hatalı Veri Girdiniz !");
             // return kullanmamızın sebebi hesaplamaya devam etmemesi için
         return;
         }
 
-        if (Yaş < 12) {
+        //Bizden istenilene göre önce önce yaş indirimi sonra yolculuk tipi indirimi yapmamızı istiyor ve bizde ona göre yapıcaz.
+
+        if (age < 12) {
             // bu durumda %50 indirim isteniyor
-            System.out.println("Toplam Tutar: " + fiyat * 0.5);
+            System.out.println("Toplam Tutar: " + price * 0.5);
         }
-        else if (Yaş >=12 && Yaş<=24) {
+        else if (age >=12 && age<=24) {
             // bu durumda %10 indirim isteniyor
-            System.out.println("Toplam Tutar: " + fiyat * 0.9);
+            System.out.println("Toplam Tutar: " + price * 0.9);
         }
-        else if (Yaş > 65) {
+        else if (age > 65) {
             // bu durumda %30 indirim isteniyor
-            System.out.println("Toplam Tutar: " + fiyat * 0.7);
+            System.out.println("Toplam Tutar: " + price * 0.7);
         }
 
         else {
-            System.out.println("Toplam Tutar: " + fiyat);
+            System.out.println("Toplam Tutar: " + price);
         }
 
-
-
-
             }
-
 
         }
 
