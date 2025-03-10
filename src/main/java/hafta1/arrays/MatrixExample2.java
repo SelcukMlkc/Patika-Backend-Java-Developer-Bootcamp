957cef30d6de1a5bf846cce739c5c935c3d618bb
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-10 16:25:26 +0300

    I did the Matrix Transpose homework

---
+++ src/main/java/hafta1/arrays/MatrixExample2.java
@@ -0,0 +1,26 @@
+package hafta1.arrays;
+
+public class MatrixExample2 {
+
+    public static void main(String[] args) {
+
+        //İstediğiniz boyutta matris oluşturarak matris değerlerine 0-99 arası random sayılar üreterek matrise atama
+
+        int[][] matrix = new int[3][4];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = (int) (Math.random() * 100);
+            }
+        }
+
+        // Aşağıdaki kod bloğunun çıktısı nedir?
+
+        int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
