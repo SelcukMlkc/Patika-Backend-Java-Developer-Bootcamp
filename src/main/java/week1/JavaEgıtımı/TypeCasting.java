5fbe9e9d2f10063e1d65c2eb7e096c6586960c49
SELÇUK <selcuk_mlkc@hotmail.com>
2025-03-13 14:03:09 +0300

    I made some additions

--- src/main/java/week1/JavaEgıtımı/TypeCasting.java
+++ src/main/java/week1/JavaEgıtımı/TypeCasting.java
@@ -16,6 +16,23 @@ public class TypeCasting {
 
         short sayiShort = (short) sayiDouble;  // bu şekilde devam etmesini sağlıyoruz  // Explicit Narrowing
 
+        String sayi = "100";
+        int tamSayı = Integer.parseInt(sayi);  //stringer tanımlı bir sayıyı integera çevirdik bu yöntemle
+
+        System.out.println(tamSayı);
+
+        String sayi2 = "35.44";
+        double ondalıklıSayi = Double.parseDouble(sayi2);
+
+        System.out.println(ondalıklıSayi);
+
+        String yaziDegeri = String.valueOf(tamSayı);  //tam tersini yaptık, int degeriini stringe dönüştürdük
+
+        System.out.println(yaziDegeri);
+
+
+
+
 
     }
 }
--- src/main/java/week1/arrays/ArrayExample.java
+++ src/main/java/week1/arrays/ArrayExample.java
@@ -1,5 +1,7 @@
 package week1.arrays;
 
+import java.util.Arrays;
+
 public class ArrayExample {
 
     public static void main(String[] args) {
@@ -8,13 +10,23 @@ public class ArrayExample {
 
         int max = numbers[0];  // burada max sayı için istediğini seçebilirdin farketmez
 
-        for (int number : numbers) {
+        for (int number : numbers) {   //bu for each döngüsü. numbers daki her elemanın sırayla number a atıyor
 
             if (number > max) {
                 max = number;
             }
+            System.out.println(number);
         }
             System.out.println("Dizideki en büyük eleman = " + max);
+        System.out.println(Arrays.toString(numbers));   // Dizileri bu şekilde yazdırabilirisin
+
+        System.out.println(numbers[1]);
+
+        //eğer ben diziyi küçükten büyüğe sıralı yazmak istersem;
+
+        Arrays.sort(numbers);
+        System.out.println("Sıralı dizi = " + Arrays.toString(numbers));
+
 
     }
 
